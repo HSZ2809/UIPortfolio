dcfde2485e9340e5b4e1043e6536d9ee847f2293
HSZ2809 <6255plm@gmail.com>
2022-07-28 18:05:00 +0900

    first commit

---
+++ Assets/02.Scripts/SceneMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMover : MonoBehaviour
+{
+    // private method
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // public method
+    public void StartScene()
+    {
+        StartCoroutine("LoadScene");
+    }
+    
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(3);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync("Lobby");
+
+        yield return true;
+    }
+}
